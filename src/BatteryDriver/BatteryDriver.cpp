86fd764222bb98fc7051b5feeb296e34a37eb64a
Judicael-MARCHAND <judicael.marchand@gmail.com>
2023-02-22 21:42:52 +0100

    Battery measure in now averaged to avoid noise because of WiFi

--- src/BatteryDriver/BatteryDriver.cpp
+++ src/BatteryDriver/BatteryDriver.cpp
@@ -9,6 +9,8 @@ BatteryDriver::BatteryDriver(void)
     mBatteryADCValue = 0;
     mBatteryVoltageInmV = 0;
     mBatterySOC = 0;
+    memset(mMeanBatteryADCValue, BATTERY_ADC_INVALID_VALUE_U16, sizeof(mMeanBatteryADCValue));
+    mMeanBatteryADCIndex = 0;
 }
 
 BatteryDriver::~BatteryDriver(void)
@@ -24,8 +26,18 @@ BatteryDriver::~BatteryDriver(void)
  */
 void BatteryDriver::execute(void)
 {
-    mBatteryADCValue = analogRead(BATTERY_ADC_PIN);
-    mBatteryVoltageInmV = ADCRawVoltageToADCInputVoltage(ADCValueToADCVoltage(mBatteryADCValue));
+    addBatteryADCValue(analogRead(BATTERY_ADC_PIN));
+    mBatteryADCValue = getBatteryADCMean();
+    mBatteryVoltageInmV = ADCRawVoltageToADCInputVoltage_mV(ADCValueToADCVoltage_mV(mBatteryADCValue));
+    // Window the battery voltage
+    if(BATTERY_FULL_CHARGE_VOLTAGE < mBatteryVoltageInmV)
+    {
+        mBatteryVoltageInmV = BATTERY_FULL_CHARGE_VOLTAGE;
+    }
+    else if(BATTERY_LOW_CHARGE_VOLTAGE > mBatteryVoltageInmV)
+    {
+        mBatteryVoltageInmV = BATTERY_LOW_CHARGE_VOLTAGE;
+    }
     mBatterySOC = map(mBatteryVoltageInmV, BATTERY_LOW_CHARGE_VOLTAGE, BATTERY_FULL_CHARGE_VOLTAGE, 0, 100);
 }
 
@@ -48,4 +60,50 @@ uint16_t BatteryDriver::getBatteryVoltageInmV(void)
 uint8_t BatteryDriver::getBatterySOC(void)
 {
     return mBatterySOC;
+}
+
+/**
+ * @brief Add ADC battery value to the data array to calculate the average value
+ * 
+ * @param value ADC value to add
+ */
+void BatteryDriver::addBatteryADCValue(uint16_t value)
+{
+    mMeanBatteryADCIndex = (mMeanBatteryADCIndex + 1) % NB_SAMPLE_BATTERY_AVERAGE;
+    mMeanBatteryADCValue[mMeanBatteryADCIndex] = value;
+}
+
+/**
+ * @brief Calculate and return the battery mean ADC measure
+ * 
+ * @return  The battery mean ADC measure
+ */
+uint16_t BatteryDriver::getBatteryADCMean(void)
+{
+    uint8_t i = 0;
+    uint8_t currentIndex = mMeanBatteryADCIndex;
+    uint8_t nbMeasure = 0;
+    uint32_t meanValue = 0;
+
+    do
+    {
+        if(BATTERY_ADC_INVALID_VALUE_U16 != mMeanBatteryADCValue[currentIndex]) // Check data validity
+        {
+            meanValue += mMeanBatteryADCValue[currentIndex];
+            ++nbMeasure;
+        }
+        // else BATTERY_ADC_INVALID_VALUE_U16 == mMeanBatteryADCValue[currentIndex] --> Don't take invalid data
+
+        // Check if we are at the origin of the array
+        if(0 == currentIndex)
+            currentIndex = NB_SAMPLE_BATTERY_AVERAGE - 1; // Reset the index to the end of the array
+
+        ++i;
+
+    } while (i < NB_SAMPLE_BATTERY_AVERAGE);
+    
+    if(0 != nbMeasure)
+        meanValue /= nbMeasure;
+    
+    return meanValue;
 }
--- src/BatteryDriver/BatteryDriver.hpp
+++ src/BatteryDriver/BatteryDriver.hpp
@@ -4,10 +4,12 @@
 #include "Arduino.h"
 
 #define BATTERY_ADC_PIN A0
-#define ADCValueToADCVoltage(adcValue) (adcValue * 1.0 / 1023.0)    // On the ESP8266 ADC, numeric value of 1023 is equaled to an input of 1V
-#define ADCRawVoltageToADCInputVoltage(adcRawVoltage) (adcRawVoltage * (390.0 + 100.0) / 100.0)   // Voltage bridge divider with Re = 390komhs and Rs = 100kohms
+#define ADCValueToADCVoltage_mV(adcValue) (adcValue * 1000.0 / 1023.0)    // On the ESP8266 ADC, numeric value of 1023 is equaled to an input of 1V
+#define ADCRawVoltageToADCInputVoltage_mV(adcRawVoltage_mV) (adcRawVoltage_mV * (390.0 + 100.0) / 100.0)   // Voltage bridge divider with Re = 390komhs and Rs = 100kohms
 #define BATTERY_LOW_CHARGE_VOLTAGE 3500
 #define BATTERY_FULL_CHARGE_VOLTAGE 4200
+#define NB_SAMPLE_BATTERY_AVERAGE 100
+#define BATTERY_ADC_INVALID_VALUE_U16 0xFFFF
 
 /**
  * @class BatteryDriver
@@ -17,9 +19,14 @@
 class BatteryDriver
 {
 private:
-uint8_t mBatteryADCValue;
-uint16_t mBatteryVoltageInmV;
-uint8_t mBatterySOC;
+  uint16_t mBatteryADCValue;
+  uint16_t mBatteryVoltageInmV;
+  uint8_t mBatterySOC;
+  uint16_t mMeanBatteryADCValue[NB_SAMPLE_BATTERY_AVERAGE];
+  uint8_t mMeanBatteryADCIndex;
+
+  void addBatteryADCValue(uint16_t value);
+  uint16_t getBatteryADCMean(void);
 
 public:
   BatteryDriver(void);
